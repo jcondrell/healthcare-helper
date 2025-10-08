881c3f72f8d70e8072e13a8e802601535891d9c1
Ryan Taylor <rhtaylor@mail.wlu.edu>
2025-10-08 16:06:12 -0400

    added ggplot2

--- server.r
+++ server.r
@@ -1,5 +1,7 @@
 library(shiny)   # shiny comes with the sidebar page ... where you use the render calls
 library(tidyverse)
+library(ggplot2)
+
 
 healthcare_dataset <- read_csv("healthcare_dataset.csv")
 stroke_prediction <- read_csv("stroke prediction.csv")
--- ui.r
+++ ui.r
@@ -1,5 +1,6 @@
 library(shiny)   # shiny comes with the sidebar page ... ui defines where outputs and inputs are on the webpage
 library(tidyverse)
+library(ggplot2)
 
 healthcare_dataset <- read_csv("healthcare_dataset.csv")
 stroke_prediction <- read_csv("stroke prediction.csv")
