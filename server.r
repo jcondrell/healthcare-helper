4d0bd1f9d3ddc01ccc52d5e3acac5c7ff9311783
Jade Condrell <jcondrell@mail.wlu.edu>
2025-10-08 15:53:35 -0400

    hi

--- server.r
+++ server.r
@@ -1,1 +1,4 @@
 library(shiny)   # shiny comes with the sidebar page ... where you use the render calls
+
+healthcare_dataset <- read_csv("healthcare_dataset.csv")
+stroke_prediction <- read_csv("stroke prediction.csv")
--- ui.r
+++ ui.r
@@ -1,1 +1,4 @@
 library(shiny)   # shiny comes with the sidebar page ... ui defines where outputs and inputs are on the webpage
+
+healthcare_dataset <- read_csv("healthcare_dataset.csv")
+stroke_prediction <- read_csv("stroke prediction.csv")
