44d3d7c2ece50b31de230f855ceb64fcb572f44b
Ryan Taylor <rhtaylor@mail.wlu.edu>
2025-10-08 15:57:10 -0400

    organizing

--- server.r
+++ server.r
@@ -2,3 +2,4 @@ library(shiny)   # shiny comes with the sidebar page ... where you use the rende
 
 healthcare_dataset <- read_csv("healthcare_dataset.csv")
 stroke_prediction <- read_csv("stroke prediction.csv")
+
