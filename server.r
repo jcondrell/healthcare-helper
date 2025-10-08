45f3f4d63b5ad7a6b18943d53d67eca4d219a354
Ryan Taylor <rhtaylor@mail.wlu.edu>
2025-10-08 16:01:25 -0400

    added tidyverse lines in server.r and ui.r

--- server.r
+++ server.r
@@ -1,4 +1,5 @@
 library(shiny)   # shiny comes with the sidebar page ... where you use the render calls
+library(tidyverse)
 
 healthcare_dataset <- read_csv("healthcare_dataset.csv")
 stroke_prediction <- read_csv("stroke prediction.csv")
--- ui.r
+++ ui.r
@@ -1,4 +1,5 @@
 library(shiny)   # shiny comes with the sidebar page ... ui defines where outputs and inputs are on the webpage
+library(tidyverse)
 
 healthcare_dataset <- read_csv("healthcare_dataset.csv")
 stroke_prediction <- read_csv("stroke prediction.csv")
