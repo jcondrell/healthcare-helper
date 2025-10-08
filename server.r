7bef203fb2bbfe84245ed7447bf60bf0c42564e6
Ryan Taylor <rhtaylor@mail.wlu.edu>
2025-10-08 15:27:45 -0400

    hi

--- server.r
+++ server.r
@@ -1,1 +1,1 @@
-library(shiny)   # shiny comes with the sidebar page
+library(shiny)   # shiny comes with the sidebar page ... where you use the render calls
--- ui.r
+++ ui.r
@@ -1,1 +1,1 @@
-library(shiny)   # shiny comes with the sidebar page
+library(shiny)   # shiny comes with the sidebar page ... ui defines wehere outputs and inputs are on the webpage
