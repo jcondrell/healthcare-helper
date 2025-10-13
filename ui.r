e4a490f6dad8601d20b5f6803766c04b09c4af15
Ryan Taylor <rhtaylor@mail.wlu.edu>
2025-10-13 16:11:35 -0400

    right ui

--- ui.r
+++ ui.r
@@ -5,6 +5,6 @@ library(ggplot2)
 # healthcare_dataset <- read_csv("healthcare_dataset.csv")
 # state_facts <- read_csv("state_facts.csv")
 
-data <- readRDS("new_datasets.RDS")
+data <- readRDS("NEW_datasets.RDS")
 healthcare_dataset <- data$healthcare_dataset
 state_facts <- data$state_facts
