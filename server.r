4c4eaef60f8279c9362c6b659f67cf2216d75a90
Ryan Taylor <rhtaylor@mail.wlu.edu>
2025-10-13 15:25:30 -0400

    new, better data

--- server.r
+++ server.r
@@ -3,7 +3,11 @@ library(tidyverse)
 library(ggplot2)
 
 
-healthcare_dataset <- read_csv("healthcare_dataset.csv")
-stroke_prediction <- read_csv("stroke prediction.csv")
+# healthcare_dataset <- read_csv("healthcare_dataset.csv")
+# stroke_prediction <- read_csv("stroke prediction.csv")
+
+data <- readRDS("datasets.RDS")
+healthcare_dataset <- data$healthcare_dataset
+stroke_prediction <- data$stroke_prediction
 
 function(input, output) {}
--- ui.r
+++ ui.r
@@ -2,5 +2,9 @@ library(shiny)   # shiny comes with the sidebar page ... ui defines where output
 library(tidyverse)
 library(ggplot2)
 
-healthcare_dataset <- read_csv("healthcare_dataset.csv")
-stroke_prediction <- read_csv("stroke prediction.csv")
+# healthcare_dataset <- read_csv("healthcare_dataset.csv")
+# stroke_prediction <- read_csv("stroke prediction.csv")
+
+data <- readRDS("datasets.RDS")
+healthcare_dataset <- data$healthcare_dataset
+stroke_prediction <- data$stroke_prediction
