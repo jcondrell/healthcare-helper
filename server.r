40af27d43090ee750c5ef2bda37297fb1725e1c0
Ryan Taylor <rhtaylor@mail.wlu.edu>
2025-10-15 15:35:54 -0400

    new shiny leaflet and rmd with leaflet

--- server.r
+++ server.r
@@ -1,6 +1,7 @@
 library(shiny)   # shiny comes with the sidebar page ... where you use the render calls
 library(tidyverse)
 library(ggplot2)
+library(leaflet)
 
 
 # healthcare_dataset <- read_csv("healthcare_dataset.csv")
@@ -10,4 +11,24 @@ data <- readRDS("NEW_datasets.RDS")
 healthcare_dataset <- data$healthcare_dataset
 state_facts <- data$state_facts
 
-function(input, output) {}
+#function(input, output) {}
+
+
+
+function(input,output){
+  
+  lats <- -90:90 #vector of all integers between the two
+  lons <--180:180
+  
+  btn <- input$newButton # reevalutates input every time you click this button for new input data ... not used for anything, but makes this whole expression reevaluate so important!
+  
+  output$uSMap <- renderLeaflet({
+    
+    leaflet() %>%
+      setView(lng =sample(lons, 1),
+              lat = sample(lats, 1),
+              zoom = 5) %>%
+      addTiles()    #to get map
+    
+  })
+}
--- ui.r
+++ ui.r
@@ -1,6 +1,7 @@
 library(shiny)   # shiny comes with the sidebar page ... ui defines where outputs and inputs are on the webpage
 library(tidyverse)
 library(ggplot2)
+library(leaflet)
 
 # healthcare_dataset <- read_csv("healthcare_dataset.csv")
 # state_facts <- read_csv("state_facts.csv")
@@ -8,3 +9,11 @@ library(ggplot2)
 data <- readRDS("NEW_datasets.RDS")
 healthcare_dataset <- data$healthcare_dataset
 state_facts <- data$state_facts
+
+
+
+fluidPage(
+  leafletOutput("uSMap"),
+  br(),
+  actionButton("newButton", "See which specialties are prominent in each state!")
+)
