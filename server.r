8b11358f8ae12318cb5afeae340960b476a0657d
Jade Condrell <jcondrell@mail.wlu.edu>
2025-10-27 15:42:02 -0400

    chloropleth

--- server.r
+++ server.r
@@ -5,6 +5,8 @@ library(ggplot2)
 library(leaflet)
 library(dplyr)
 library(ggpubr)
+library(sf)
+library(htmltools)
 
 
 # Importing all csv datasets: 
@@ -19,8 +21,47 @@ function(input, output) {
 
 ################################
 # 1 CHLOROPLETH:
-# geo <- geojson_read("states.geo.json", what = "sp") 
-# we will wait on this cloropleth for MONDAY!!!! - "See which specialties are prominent in each state!"
+  output$main_plot <- renderLeaflet({
+    states <- read_sf("us-states.geojson")
+    
+    bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
+    pal <- colorBin("YlOrRd", domain = states$density, bins = bins)
+    
+    labels <- sprintf(
+      "<strong>%s</strong><br/>%g people / mi<sup>2</sup>",
+      states$name, states$density
+    ) %>% lapply(HTML)
+    
+    leaflet(states) %>%
+      setView(-96, 37.8, 4) %>%
+      addProviderTiles("MapBox", options = providerTileOptions(
+        id = "mapbox.light",
+        accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+      addPolygons(
+        fillColor = ~pal(density),
+        weight = 2,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlightOptions = highlightOptions(
+          weight = 5,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.7,
+          bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto")) %>%
+      addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
+                position = "bottomright")
+    
+    
+    
+  })
+  
 #######################################
 
 
